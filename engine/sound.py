95bad4f23a626877ece199ef9e3e757648aabb5b
q1225 <46804537+q1225@users.noreply.github.com>
2022-10-14 14:51:41 -0400

    Add files via upload

---
+++ engine/sound.py
@@ -0,0 +1,84 @@
+def voice(scence, n, x, y):
+    scence = vc
+    for i in range(0, len(vc)):
+        sound = pygame.mixer.load(vc[i])
+    for c in range(0, n):
+        channel3 = pygame.mixer.Channel(c)
+        pygame.mixer.Channel.play()
+        pygame.mixer.Channel.stop()
+        return print(lines)
+    with open("Cult_of_Seal_VN_lines_Final.docx", "r") as fl:
+        speech = create_surface_with_text(
+            "Cult_of_Seal_VN_lines Final.docx", 32, ORANGE, MB
+        )
+    for x in range(x + n, y, +2):
+        lines = [line.rstrip() for line in fl]
+    else:
+        pygame.image.load(C[ch + 1])
+        X += 100
+        Y = 400
+        move_to_front()
+        pygame.time.set_timer(5000)
+        display_surface.blit(image, (0, 0))
+        sound = pygame.mixer.load(vc[ch[i]])
+        channel3 = pygame.mixer.Channel(n)
+        pygame.mixer.Channel.play()
+        pygame.mixer.Channel.stop()
+        with open("Cult_of_Seal_VN_lines_Final.docx", "r") as fl:
+         speech = create_surface_with_text(
+            "Cult_of_Seal_VN_lines_Final.docx", 32, ORANGE, MB
+        )
+        for x1 in range(x1 + 1, y1, +2):
+         lines = [line.rstrip() for line in fl]
+
+        for cp in range(0, n):
+         ch = input("who")
+         f = input("How is the charater feeling")
+         feeling(f, ch, x, y)
+        # minor characters
+        if ch == 5:
+         sc = [127, 129]
+        with open("Cult_of_Seal_VN_lines_Final.docx", "r") as fl:
+            for x2 in range(0, len(sc1)):
+                lines = [line.rstrip() for line in fl]
+                return print(lines)
+        if ch == 6:
+         sc1 = [128, 130]
+        with open("Cult_of_Seal_VN_lines_Final.docx", "r") as fl:
+            for x3 in range(0, len(sc2)):
+                lines = [line.rstrip() for line in fl]
+                return print(lines)
+        if ch == 7:
+         sc = [241, 243, 245, 247]
+        with open("Cult_of_Seal_VN_lines_Final.docx", "r") as fl:
+            for x4 in range(0, len(sc3)):
+                lines = [line.rstrip() for line in fl]
+                return print(lines)
+        if ch == 8:
+         sc = [260, 264]
+         with open("Cult_of_Seal_VN_lines_Final.docx", "r") as fl:
+            for x2 in range(0, len(sc4)):
+                lines = [line.rstrip() for line in fl]
+            return print(lines)
+def sounde(n, soundeffect, x4):
+    if soundeffect == True:
+        self.sound = pygame.mixer.load(SE[n])
+        channel2 = pygame.mixer.Channel(4)
+        pygame.mixer.Channel.play()
+        pygame.mixer.Channel.stop()
+        with open("Cult_of_Seal_VN_lines_Final.docx", "r") as fl:
+            lines = ts1[i]
+    for i in range(0, 3):
+        ts1 = [44, 31, 40]
+    if sounde(n, True, x == ts1[i]):
+        sounde(2, True, ts1[i])
+        sounde(5, True, ts1[i])
+    for xs in range(4, 270):
+        with open("Cult_of_Seal_VN_lines_Final.docx", "r") as fl:
+            lines = xs
+    self.sound = pygame.mixer.load(m(1))
+    channel1 = pygame.mixer.Channel(0)
+    pygame.mixer.Channel.play()
+    pygame.mixer.Channel.stop()
+    self.dialogue = dialog(n, x, y)
+    
