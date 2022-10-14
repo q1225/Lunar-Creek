1350c82241a061891339ccd4a6ccf7dddd4e7944
q1225 <46804537+q1225@users.noreply.github.com>
2022-10-14 17:34:20 -0400

    Delete Player.py

--- engine/Player.py
+++
@@ -1,6 +0,0 @@
-class Player:
-    """Stores information about a player"""
-
-    def __init__(self, score: int = 0, current_level: int = 1):
-        self.score = score
-        self.current_level = current_level
