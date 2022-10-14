ba88a8f74938546446832a3f4be3cee6e78c441d
q1225 <46804537+q1225@users.noreply.github.com>
2022-10-14 14:56:48 -0400

    Update save_load.py

--- engine/save_load.py
+++ engine/save_load.py
@@ -9,7 +9,22 @@ def save_btn(self, data, name):
         return tuple(variables)
     else:
         return variables[0]                       
-def notebook(scence):
+def load_btn(name: str):
+    with open(os.path.join(SAVE_FOLDER, f"{name}.data"), "rb") as data_file:
+        return pickle.load(data_file)
+
+    # variables = []
+    # for index, file in enumerate(files_to_load):
+    #     if self.check_for_file(file):
+    #         variables.append(self.load_data(file))
+    #     else:
+    #         variables.append(default_data[index])
+    #     if len(variables) > 1:
+    #         return tuple(variables)
+    #     else:
+    #         return variables[0]
+                             
+    def notebook(scence):
     if scence == 4:
         pygame.time.set_timer(5000)
         gamestate.notebook
