47b5f1954e3665c617d08e82a3d991e78221d1ec
q1225 <46804537+q1225@users.noreply.github.com>
2022-10-14 14:48:14 -0400

    Add files via upload

---
+++ engine/speech and character.py
@@ -0,0 +1,81 @@
+def dialog(n, x2, y2):
+    with open("Cult_of_Seal_VN_lines_Final.docx", "r") as fl:
+        speech = textOutput(line)
+    for x in range(x + n, y, +2):
+        lines = [line.rstrip() for line in fl]
+    if n % 2 != 0:
+        pygame.image.load(C[ch])
+        X = 700
+        Y = 400
+        move_to_front()
+        pygame.time.set_timer(5000)
+        display_surface.blit(image, (0, 0))
+        return print(lines)
+        with open("Cult_of_Seal_VN_lines_Final.docx", "r") as fl:
+            speech = textOutput(line)
+            for x1 in range(x1 + 1, y1, +2):
+                line = [line.rstrip() for line in fl]
+    else:
+        pygame.image.load(C[ch + 1])
+        X += 100
+        Y = 400
+        move_to_front()
+        pygame.time.set_timer(5000)
+        display_surface.blit(image, (0, 0))
+        sound = pygame.mixer.load(vc[ch[i]])
+        channel3 = pygame.mixer.Channel(n)
+        pygame.mixer.Channel.play()
+        pygame.mixer.Channel.stop()
+        return print(line)
+        with open("Cult_of_Seal_VN_lines_Final.docx", "r") as fl:
+            speech = textOutput(line)
+            for x1 in range(x1 + 1, y1, +2):
+                line = [line.rstrip() for line in fl]
+def feeling(f, ch, x1, y1):
+        # n==1 fannie f==cp==0-5
+        # n==2 bea f==cp==6-11
+        # n==3 innkeeper f==cp==13-19
+        # n==4 musuem guide f==cp== 20
+        # n==5 boss f==cp==12
+        # n==6  f==cp==
+        for a in range(0, f):
+            if f != 0:
+                pygame.image.load(C[ch + f])
+            if n % 2 != 0:
+                X += 100
+                Y = 200
+                move_to_front()
+                pygame.time.set_timer(5000)
+                display_surface.blit(image, (0, 0))
+                sound = pygame.mixer.load(vc[i])
+            else:
+                X = 700
+                Y = 200
+                move_to_front()
+                pygame.time.set_timer(5000)
+                display_surface.blit(image, (0, 0))
+              # minor characters
+            if ch == 5:
+         sc = [127, 129]
+        with open("Cult_of_Seal_VN_lines_Final.docx", "r") as fl:
+            for x2 in range(0, len(sc1)):
+                lines = [line.rstrip() for line in fl]
+                return print(lines)
+        if ch == 6:
+         sc1 = [128, 130]
+        with open("Cult_of_Seal_VN_lines_Final.docx", "r") as fl:
+            for x3 in range(0, len(sc2)):
+                lines = [line.rstrip() for line in fl]
+                return print(lines)
+        if ch == 7:
+         sc = [241, 243, 245, 247]
+        with open("Cult_of_Seal_VN_lines_Final.docx", "r") as fl:
+            for x4 in range(0, len(sc3)):
+                lines = [line.rstrip() for line in fl]
+                return print(lines)
+        if ch == 8:
+         sc = [260, 264]
+         with open("Cult_of_Seal_VN_lines_Final.docx", "r") as fl:
+            for x2 in range(0, len(sc4)):
+                lines = [line.rstrip() for line in fl]
+            return print(lines)
