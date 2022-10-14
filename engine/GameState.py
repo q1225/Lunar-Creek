cfb791e9ec607e6d84d20a7d1e940966bce2fc92
q1225 <46804537+q1225@users.noreply.github.com>
2022-10-14 17:39:04 -0400

    Add files via upload

---
+++ engine/GameState.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class GameState(Enum):
+    QUIT = -1
+    TITLE = 0
+    NEW_GAME = 1
+    NEXT_LEVEL = 2
+    NEXT_BACKGROUND=7
+    NEXT_LINE=8
+    OPTIONS = 3
+    Notebook = 4
+    Credits = 5
+    LOAD = 6
