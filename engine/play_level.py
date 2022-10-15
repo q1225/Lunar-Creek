9721bb50d119c839d8d6ff5cf5a9b2b747d24563
q1225 <46804537+q1225@users.noreply.github.com>
2022-10-15 12:40:25 -0400

    Update play_level.py

--- engine/play_level.py
+++ engine/play_level.py
@@ -14,8 +14,24 @@ def play_level():
         text_rgb=MIDNIGHT_BLUE,
         text="Return to options",
         action=GameState.OPTIONS,
-    )
-
+      )
+    nextbackground_btn = UIElement(
+            center_position=(400, 700),
+            font_size=30,
+            bg_rgb=WHITE,
+            text_rbg=LIME_GREEN,
+            text="next bg",
+            action=GameState.NEXT_BACKGROUND,
+            )
+     next_line_btn=UIElement(
+        center_postion=(700,700),
+        font_size=30,
+         bg_rgb=WHITE,
+        text_rgb=ORANGE,
+        text="next line",
+        action=GameState.NEXT_LINE,
+        )
+   
     next_level_btn = UIElement(
         center_position=(400, 400),
         font_size=30,
