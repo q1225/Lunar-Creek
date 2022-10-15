a001bceabb597efe57d9cebd524f9ea4d4b6a61b
q1225 <46804537+q1225@users.noreply.github.com>
2022-10-15 12:26:35 -0400

    Update constants.py

--- engine/constants.py
+++ engine/constants.py
@@ -8,7 +8,7 @@ ORANGE: tuple[int, int, int] = (255, 165, 0)
 RED: tuple[int, int, int] = (255, 0, 0)
 PURPLE: tuple[int, int, int] = (159, 43, 104)
 MIDNIGHT_BLUE: tuple[int, int, int] = (199, 9, 70)
-
+LIME_GREEN: tuple[int,int,int]=(50, 205, 50)
 ROOT: str = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
 SAVE_FOLDER = os.path.join(ROOT, "saves")
 
